54877445e6612eb5db8186f456436bca7b7700d5
rustam akimov <akirusprod@gmail.com>
2022-07-15 12:08:03 +0300

    dense input data shape bug fix

--- nnmodel/layers/dense.py
+++ nnmodel/layers/dense.py
@@ -45,7 +45,9 @@ class Dense():
         self.output_shape = (1, self.units_num)
 
     def forward_prop(self, X, training):
-        self.input_data = np.squeeze(X)
+        self.input_data = X
+        if len(X.shape) == 3 and X.shape[1] == 1:
+            self.input_data = np.squeeze(X)
        
         self.batch_size = len(self.input_data)
 
