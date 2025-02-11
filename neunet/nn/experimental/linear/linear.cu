24212ecb870177d3ff0f2b6f003fda0c585be8a8
AkiRusProd <akirusprod@gmail.com>
2025-02-11 20:56:18 +0300

    core refactoring

--- neunet/nn/experimental/linear/linear.cu
+++ neunet/nn/experimental/linear/linear.cu
@@ -49,49 +49,6 @@ __global__ void sumBiasKernel(float *d_bias, const float *d_output, int rowsNum,
     }
 }
 
-// C(rowsNum, colsNum) = A(rowsNum, width) * B(width, colsNum) + bias
-void blasMatMulWithBias(const float *A, const float *B, const float *bias, float *C, const int rowsNum, const int width, const int colsNum) {
-    const float alf = 1.0f;
-    const float bet = 0.0f;
-
-    cublasHandle_t handle;
-    cublasCreate(&handle);
-
-    // matrix multiplication: C = A * B^T
-    cublasSgemm(handle,
-                CUBLAS_OP_T,   // Transpose A (A is originally rowsNum x width in row-major, becomes width x rowsNum)
-                CUBLAS_OP_N,   // No transpose B (B is colsNum x width in row-major)
-                colsNum,       // m: rows of op(A) and C (colsNum)
-                rowsNum,       // n: columns of op(B) and C (rowsNum)
-                width,         // k: columns of op(A) and rows of op(B) (width)
-                &alf,
-                B,             // A: originally B (colsNum x width in row-major)
-                width,         // lda: leading dimension of A (width, since row-major)
-                A,             // B: originally A (rowsNum x width in row-major)
-                width,         // ldb: leading dimension of B (width, since row-major)
-                &bet,
-                C,             // C: result matrix (rowsNum x colsNum in row-major)
-                colsNum);      // ldc: leading dimension of C (colsNum, since row-major)
-
-
-    // Add bias to each column of C
-    if (bias != nullptr) {
-        // C(i, j) += bias(j)
-        // int threadsPerBlock = 256;  // You can experiment with this value
-        // int blocksPerGrid = (rowsNum + threadsPerBlock - 1) / threadsPerBlock;
-
-        dim3 threadsPerBlock(16, 16);
-        dim3 blocksPerGrid((rowsNum + 15) / 16, (colsNum + 15) / 16);
-
-        // Add bias to each row for each column (without race conditions)
-        addBiasKernel<<<blocksPerGrid, threadsPerBlock>>>(C, bias, rowsNum, colsNum);
-        cudaDeviceSynchronize();  // Make sure the kernel execution finishes
-    }
-
-    cublasDestroy(handle);
-}
-
-
 extern "C" {
     DLLEXPORT void cudaLinearModuleForward(float *input, float *weights, float *bias, float *output, int inputRowsNum, int inputColsNum, int outputColsNum) {
         float *p_input, *p_weights, *p_bias, *p_output;
@@ -114,7 +71,41 @@ extern "C" {
         cudaMemcpy(p_weights, weights, inputColsNum * outputColsNum * sizeof(float), cudaMemcpyHostToDevice);
 
         // Perform linear layer operation: output = input * weights + bias
-        blasMatMulWithBias(p_input, p_weights, p_bias, p_output, inputRowsNum, inputColsNum, outputColsNum);
+        // C(rowsNum, colsNum) = A(rowsNum, width) * B(width, colsNum) + bias
+        const float alf = 1.0f;
+        const float bet = 0.0f;
+
+        cublasHandle_t handle;
+        cublasCreate(&handle);
+
+        // matrix multiplication: C = A * B^T
+        cublasSgemm(handle,
+                    CUBLAS_OP_T,   // Transpose A (A is originally inputRowsNum x inputColsNum in row-major, becomes inputColsNum x inputRowsNum)
+                    CUBLAS_OP_N,   // No transpose B (B is outputColsNum x inputColsNum in row-major)
+                    outputColsNum, // m: rows of op(A) and C (outputColsNum)
+                    inputRowsNum,  // n: columns of op(B) and C (inputRowsNum)
+                    inputColsNum,  // k: columns of op(A) and rows of op(B) (inputColsNum)
+                    &alf,
+                    p_weights,     // A: originally B (outputColsNum x inputColsNum in row-major)
+                    inputColsNum,  // lda: leading dimension of A (inputColsNum, since row-major)
+                    p_input,       // B: originally A (inputRowsNum x inputColsNum in row-major)
+                    inputColsNum,  // ldb: leading dimension of B (inputColsNum, since row-major)
+                    &bet,
+                    p_output,      // C: result matrix (inputRowsNum x outputColsNum in row-major)
+                    outputColsNum);// ldc: leading dimension of C (outputColsNum, since row-major)
+
+        // Add bias to each column of C
+        if (p_bias != nullptr) {
+            // C(i, j) += bias(j)
+            dim3 threadsPerBlock(16, 16);
+            dim3 blocksPerGrid((inputRowsNum + 15) / 16, (outputColsNum + 15) / 16);
+
+            // Add bias to each row for each column (without race conditions)
+            addBiasKernel<<<blocksPerGrid, threadsPerBlock>>>(p_output, p_bias, inputRowsNum, outputColsNum);
+            cudaDeviceSynchronize();  // Make sure the kernel execution finishes
+        }
+
+        cublasDestroy(handle);
 
         // Copy result back to host
         cudaMemcpy(output, p_output, inputRowsNum * outputColsNum * sizeof(float), cudaMemcpyDeviceToHost);
